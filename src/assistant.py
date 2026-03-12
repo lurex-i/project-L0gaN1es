79a71ecbce262c5826ac8220ead798a10ba5f7c6
iryna_kovalenko <ikova1306@gmail.com>
2026-03-12 20:51:24 +0100

    email and address classes added

--- src/assistant.py
+++ src/assistant.py
@@ -10,14 +10,14 @@ def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
+        except Exception as e:
+            return f"{e}"
         except ValueError:
             return "Enter the correct argument for the command."
         except KeyError:
             return "There's no such user in the phonebook."
         except IndexError:
-            return "Enter contact's name after the command."
-        except Exception as e:
-            return f"{e}"
+            return "Enter contact's name after the command." 
     return inner
 
 @input_error
@@ -50,6 +50,48 @@ def show_phone(args, book:AddressBook):
     if record == None:
         return "There is no such contact in the address book."
     return f"{name} : {book[name]}."
+
+@input_error
+def add_email(args, book:AddressBook):
+    name, email, *_ = args
+    record = book.find(name)
+    message = ""
+    if record == None:
+        record = Record(name)
+        book.add_record(record)
+        message += "New contact was created. "
+    record.add_email(email)
+    message += f"Email at {record.email} for {name} was added."
+    return message
+
+@input_error
+def show_email(args, book:AddressBook):
+    name, *_ = args
+    record = book.find(name)
+    if record == None:
+        return f"There is no {name} in the address book."
+    return f"{name} : {record.email}"
+
+@input_error
+def add_address(args, book:AddressBook):
+    name, address, *_ = args
+    record = book.find(name)
+    message = ""
+    if record == None:
+        record = Record(name)
+        book.add_record(record)
+        message += "New contact was created. "
+    record.add_address(address)
+    message += f"Address at {record.address} for {name} was added."
+    return message
+
+@input_error
+def show_address(args, book:AddressBook):
+    name, *_ = args
+    record = book.find(name)
+    if record == None:
+        return f"There is no {name} in the address book."
+    return f"{name} : {record.address}" 
 
 @input_error
 def add_birthday(args, book:AddressBook):
@@ -90,10 +132,10 @@ def parse_input(user_input):
 
 @input_error
 def  show_all(args, book:AddressBook):
-    message = ""
+    message = "" 
     for name, phone in book.items():
         message += f"{name} : {phone}\n"
-    return message
+    return message 
 
 commands = {
     "hello": lambda args, book: "How can I help you?",
@@ -103,7 +145,11 @@ commands = {
     "add-birthday": add_birthday,
     "show-birthday": show_birthday,
     "birthdays": birthdays,
-    "all":  show_all
+    "add-email": add_email,
+    "show-email": show_email,
+    "add-address": add_address,
+    "show-address": show_address,
+    "all": show_all
 }
 
 def main():
@@ -133,7 +179,7 @@ def main():
                 print(execution_result)
         else:
             print("Invalid command.")
-            print("Use one of: hello, add, change, phone, all, add-birthday, show-birthday, birthdays, exit/close")
+            print("Use one of: hello, add, change, phone, all, add-birthday, show-birthday, birthdays, add-email, show-email, add-address, show-address, exit/close")
 
 
 if __name__ == "__main__":
